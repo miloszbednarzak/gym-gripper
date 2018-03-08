aee13e40f5f9e119a67ef5ef6e2fb314965ff0f8
miloszbednarzak <milosz.bedanrzak@gmail.com>
2018-03-08 15:11:46 +0100

    Adding documentation and action space

--- environment/enviornment.py
+++ environment/enviornment.py
@@ -2,8 +2,53 @@ import pygame
 import random
 # random.seed(108)
 
-# Initializing game engine
-pygame.init()
+
+def gripper_joints(wrist_pos_x, wrist_pos_y):
+
+    """
+    Define what are gripper joints coordinates based on position of gripper wrist.
+    Joints coorditates when gripper is positioned in initial position:
+    [[x-4,y-18],[x-6,y-14],[x-6,y-4],[x, y],[x+6,y-4],[x+6,y-14],[x+4,y-18]]
+
+    :param wrist_pos_x: co-ordinates of wrist on x-axis
+    :param wrist_pos_y: co-ordinates of wrist on y-axis
+    :return: coordinates od joints
+    """
+
+    joints_loc = [                                # _________
+        [wrist_pos_x - 4, wrist_pos_y - 18],     # / ________\
+        [wrist_pos_x - 6, wrist_pos_y - 14],    # //         \\
+        [wrist_pos_x - 6, wrist_pos_y - 4],    # // left finger co-ordinates
+        [wrist_pos_x, wrist_pos_y],           # ||  wrist position
+        [wrist_pos_x + 6, wrist_pos_y - 4],    # \\ right finger co-ordinates
+        [wrist_pos_x + 6, wrist_pos_y-14],      # \\_________//
+        [wrist_pos_x + 4, wrist_pos_y - 18]      # \_________/
+    ]
+    return joints_loc
+
+
+def draw_gripper(joints_positions):
+
+    """
+    Draw gripper based on joint coordinates.
+
+    :param joints_positions: co-ordinates of gripper joints
+
+    """
+    pygame.draw.lines(screen, LIGHT, False, joints_positions, 3)
+
+
+def draw_circle(position, circle_size):
+
+    """
+    Draw dark colored circle of some size in some position.
+
+    :param position: co-ordinates of circle center
+    :param circle_size: size of circle
+    """
+
+    pygame.draw.circle(screen, DARK, position, circle_size)
+
 
 # Define colors
 BLACK = (  0,   0,   0)
@@ -11,39 +56,41 @@ DARK =  ( 85,  85,  85)
 LIGHT = (170, 170, 170)
 WHITE = (255, 255, 255)
 
-# gripper_joints = [[x-4,y-18],[x-6,y-14],[x-6,y-4],[x, y],[x+6,y-4],[x+6,y-14],[x+4,y-18]]
-
 # Gripper wrist position
 wrist_position_x = 100
 wrist_position_y = 195
 
+# Gripper movement velocity
 gripper_x_velocity = 0
 gripper_y_velocity = 0
 
 
-def gripper_joints(wrist_pos_x, wrist_pos_y):
-    """ Define what are gripper joints positions based on position of """
-    joints_loc = [[wrist_pos_x - 4, wrist_pos_y - 18], [wrist_pos_x - 6, wrist_pos_y - 14],
-                 [wrist_pos_x - 6, wrist_pos_y - 4], [wrist_pos_x, wrist_pos_y],
-                 [wrist_pos_x + 6, wrist_pos_y - 4], [wrist_pos_x + 6, wrist_pos_y-14],
-                 [wrist_pos_x + 4, wrist_pos_y - 18]]
-    return joints_loc
-
-
-def draw_gripper(gripper_joints):
-    pygame.draw.lines(screen, LIGHT, False, gripper_joints, 3)
-
-
-def draw_circle(position, size):
-    pygame.draw.circle(screen, DARK, position, size)
-
-
 # Set sizes of the screen and objects
+ # set size of screen
 size = [200, 200]  # [width, height]
-circle_position = [random.randint(20, 160), random.randint(20, 160)]  # [width, height]
+
+ # set size of action space
+action_space = {
+    0: 'GO UP',
+    1: 'GO DOWN',
+    2: 'GO LEFT',
+    3: 'GO RIGHT',
+    # 5: 'TURN CLOCKWISE',
+    # 6: 'TURN COUNTER-CLOCKWISE',
+    # 7: 'TIGHTEN FINGERS',
+    # 8: 'EXTEND FINGERS'
+}
+
+ # set size of observation space alias board
 board = [20, 20, 160, 160]  # [x, y, width, height]
 
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+circle_position = [random.randint(20, 160), random.randint(20, 160)]  # [width, height]
+
+
+# Initializing game engine
+pygame.init()
+
+screen = pygame.display.set_mode(size)  # add pygame.RESIZABLE to arguments for resizable screen
 
 # Set window name
 pygame.display.set_caption("2D grasping environment")
@@ -54,37 +101,42 @@ clock = pygame.time.Clock()
 done = False
 
 # Main Program Loop
-while done == False:
+while not done:
 
     # --EVENT PROCESSING--
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                print('Key left')
-                gripper_x_velocity = -3
-            if event.key == pygame.K_RIGHT:
-                gripper_x_velocity = 3
-                print('Key right')
-            if event.key == pygame.K_UP:
-                gripper_y_velocity = -3
-                print('Key up')
-            if event.key == pygame.K_DOWN:
-                gripper_x_velocity = 3
-                print('Key down')
-
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                gripper_x_velocity = 0
-            if event.key == pygame.K_d:
-                gripper_x_velocity = 0
-            if event.key == pygame.K_w:
-                gripper_y_velocity = 0
-            if event.key == pygame.K_s:
-                gripper_x_velocity = 0
 
     # --LOGIC--
+
+    action = random.sample(action_space.keys(), 1)[0]  # get random action from action space dictionary
+    print(action)
+    if action == 0:
+        gripper_y_velocity = -3
+        print('Key up')
+    if action == 1:
+        gripper_x_velocity = 3
+        print('Key down')
+    if action == 2:
+        print('Key left')
+        gripper_x_velocity = -3
+    if action == 3:
+        gripper_x_velocity = 3
+        print('Key right')
+
+
+    # elif event.type == pygame.KEYUP:
+    #     if event.key == pygame.K_a:
+    #         gripper_x_velocity = 0
+    #     if event.key == pygame.K_d:
+    #         gripper_x_velocity = 0
+    #     if event.key == pygame.K_w:
+    #         gripper_y_velocity = 0
+    #     if event.key == pygame.K_s:
+    #         gripper_x_velocity = 0
+
     wrist_position_x += gripper_x_velocity
     wrist_position_y += gripper_y_velocity
 
